740b3af38b102f1101111d812d66c9409ea6123c
Bismark Yamoah <bismarkkwabenayamoah@gmail.com>
2025-11-04 19:25:39 +0000

    feat: implement server-side rendering configuration and routing for Angular application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MapView } from './pages/viewer/map-view';
+import { TrackView } from './pages/tracker/tracker';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MapView
+    },
+    {
+        path: 'track',
+        component: TrackView
+    }
+];
