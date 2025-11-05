9bb2e623398cffd968ad085699edca9efb969c17
Bismark Yamoah <bismarkkwabenayamoah@gmail.com>
2025-11-05 01:28:21 +0000

    refactor: remove unused imports and cleaning up code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,9 @@
 
 import { RouterOutlet } from '@angular/router';
-
-
-import { AfterViewInit, ApplicationRef, Component, effect, ElementRef, EnvironmentInjector, inject, OnDestroy, OnInit, signal, viewChild } from '@angular/core';
-import { Map, config, Marker, GeolocateControl, GeolocationType } from '@maptiler/sdk';
-import { environment } from "@/environments/env";
-import { NgClass } from '@angular/common';
-// import { SearchService } from "@services/search.service";
-import { timer } from 'rxjs';
-import { createDetachedComponent } from "@/app/lib/detached.component";
-import { CarMarker } from './components/car-marker/car-marker';
-// import { ItemMarkerComponent } from "@components/item-marker/item-marker.component";
-
+import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
-  imports: [ NgClass, RouterOutlet],
+  imports: [ RouterOutlet ],
    templateUrl: './app.html',
   styleUrl: './app.css'
 })
